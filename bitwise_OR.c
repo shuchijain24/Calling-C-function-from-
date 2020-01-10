d41fd97acd7262f670a311efc2596ff8a2db534e
shuchijain24 <shuchi.24jain@gmail.com>
2020-01-10 17:28:06 +0530

    program to perform bitwise OR operator

---
+++ bitwise_OR.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+int a, b;
+printf("Enter value for a\n");
+scanf("%d", &a);
+    
+printf("\nEnter value for b\n");
+scanf("%d", &b);
+        
+printf("Output = %d", a|b);
+return 0;
+}
